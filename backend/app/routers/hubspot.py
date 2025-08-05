5a0a76252b12e9262715c4ef9b04f1fd8db7c119
Morgan <morganzaque@hotmail.co.uk>
2025-08-05 12:16:30 +0100

    Added background tasks

--- backend/app/routers/hubspot.py
+++ backend/app/routers/hubspot.py
@@ -1,22 +1,22 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, BackgroundTasks
 from app.services import hubspot
 
 router = APIRouter(prefix="/hubspot", tags=["HubSpot"])
 
 
 @router.post("/sync")
-def sync_hubspot_data():
+def sync_hubspot_data(background_tasks: BackgroundTasks):
     try:
-        hubspot.sync_all_data()
-        return {"status": "success", "message": "HubSpot data synced"}
+        background_tasks.add_task(hubspot.sync_all_data)
+        return {"status": "started", "message": "HubSpot full sync started"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.post("/time-sync")
-def sync_hubspot_data():
+def sync_hubspot_time_data(background_tasks: BackgroundTasks):
     try:
-        hubspot.time_sync()
-        return {"status": "success", "message": "Hubspot Time data synced"}
+        background_tasks.add_task(hubspot.time_sync)
+        return {"status": "started", "message": "HubSpot time sync started"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
